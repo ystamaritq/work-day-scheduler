bb69cfc48374fad25402528037abc3e0bdeadfc4
Yadira Tamarit <ystamaritq@gmail.com>
2020-05-13 12:08:26 -0500

    added the rows with the data and buttons

--- assets/script.js
+++ assets/script.js
@@ -37,4 +37,22 @@ var calendarData = [
 	},
 ];
 
-$(document).ready(function () {});
+$(document).ready(function () {
+	//getting the current day and added a new format
+	$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+	//target tbody to manipulate the table rows ... and more
+	var tbody = $("#calendar-content");
+	//creating for each object(entry) of the array calendarData appending the rows and th,td, buttons
+	calendarData.forEach((entry) => {
+		tbody.append(`<tr style="background-color: lightgray;"><th style="background-color: whitesmoke;
+  border-right: 3px solid teal; vertical-align: middle;">${entry.hour}</th><td class="pl-0" style="width: 780px;padding-top: 0px;padding-bottom: 0px;padding-right: 0px;"><textarea  style="width: 100%; padding-top: 0px;
+  padding-bottom: 0px; padding-left: 0px; padding-right: 0px; height: 80px; border-left: 0px;" value="${entry.event}"/></td><td class="pt-0 pb-0">
+        <button data-hour="${entry.hour}" class="save_btn btn btn-lg bg-info text-white btn-outline-info waves-effect" style= "height: 80px;
+  width: 100px;"><i class="fas fa-thumbtack"
+      aria-hidden="true"></i></button></td></tr>`);
+	});
+
+	$(".save_btn").on("click", function () {
+		alert($(this).attr("data-hour"));
+	});
+});

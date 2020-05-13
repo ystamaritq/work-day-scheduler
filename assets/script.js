056cfe3b2320a098d0a62294f1c243b67885ca8e
Yadira Tamarit <ystamaritq@gmail.com>
2020-05-13 11:59:42 -0500

    script with a dataCalendar first object version could change in the future

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+var calendarData = [
+	{
+		hour: "9AM",
+		event: "empty",
+	},
+	{
+		hour: "10AM",
+		event: "",
+	},
+	{
+		hour: "11AM",
+		event: "empty",
+	},
+	{
+		hour: "12PM",
+		event: "empty",
+	},
+	{
+		hour: "1PM",
+		event: "",
+	},
+	{
+		hour: "2PM",
+		event: "empty",
+	},
+	{
+		hour: "3PM",
+		event: "empty",
+	},
+	{
+		hour: "4PM",
+		event: "empty",
+	},
+	{
+		hour: "5PM",
+		event: "empty",
+	},
+];

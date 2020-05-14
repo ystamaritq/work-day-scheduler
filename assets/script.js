20378677d766cd0d5338a163eb904341997ccc89
Yadira Tamarit <ystamaritq@gmail.com>
2020-05-14 09:55:12 -0500

    added attr data ref id to the next issue

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,50 @@
 var calendarData = [
 	{
 		hour: "9AM",
-		event: textareaInput,
+		event: "",
 		id: 9,
 	},
 	{
 		hour: "10AM",
-		event: textareaInput,
+		event: "",
 		id: 10,
 	},
 	{
 		hour: "11AM",
-		event: textareaInput,
+		event: "",
 		id: 11,
 	},
 	{
 		hour: "12PM",
-		event: textareaInput,
+		event: "",
 		id: 12,
 	},
 	{
 		hour: "1PM",
-		event: textareaInput,
+		event: "",
 		id: 13,
 	},
 	{
 		hour: "2PM",
-		event: textareaInput,
+		event: "",
 		id: 14,
 	},
 	{
 		hour: "3PM",
-		event: textareaInput,
+		event: "",
 		id: 15,
 	},
 	{
 		hour: "4PM",
-		event: textareaInput,
+		event: "",
 		id: 16,
 	},
 	{
 		hour: "5PM",
-		event: textareaInput,
+		event: "",
 		id: 17,
 	},
-	{
-		hour: "10PM",
-		event: textareaInput,
-		id: 22,
-	},
-	{
-		hour: "11PM",
-		event: textareaInput,
-		id: 23,
-	},
 ];
-
-var textareaInput = "";
 
 $(document).ready(function () {
 	//getting the current day and added a new format
@@ -66,10 +54,12 @@ $(document).ready(function () {
 
 	//creating for each object(entry) of the array calendarData appending the rows and th,td, buttons
 	calendarData.forEach((entry) => {
+		//current hour
 		var currentHour = moment().hour();
 
 		var styleClass = "";
 
+		// Develop the functionality to handle the color in each timeblock
 		if (currentHour === entry.id) {
 			styleClass = "present";
 		} else if (currentHour < entry.id) {
@@ -80,29 +70,17 @@ $(document).ready(function () {
 
 		tbody.append(`
         <tr class="rowClass"">
-         <th style="background-color: whitesmoke; border-right: 3px solid teal; vertical-align: middle;">${entry.hour}</th>
-          <td class="pl-0 ${styleClass}" style="width: 780px;padding-top: 0px;padding-bottom: 0px;padding-right: 0px;">
-          <textarea style="width: 100%; padding-top: 0px;padding-bottom: 0px; padding-left: 0px; padding-right: 0px; height: 80px; border-left: 0px;" value="${entry.event}" dataId= "${entry.id}" class="textareaClass description"/></td>
-            <td class="pt-0 pb-0 pl-0"><button data-hour="${entry.hour}" data-textareaId ="${entry.id}" class="saveBtn btn btn-lg bg-info text-white btn-outline-info waves-effect" style= "height: 80px; width: 100px;"><i class="fas fa-thumbtack" aria-hidden="true"></i></button></td>
+			<th style="background-color: whitesmoke; border-right: 3px solid teal; vertical-align: middle;">
+			 ${entry.hour}
+			</th>
+        	<td class="pl-0 ${styleClass}" style="width: 780px;padding-top: 0px;padding-bottom: 0px;padding-right: 0px;">
+				<textarea id="txt-${entry.id}" style="width: 100%; padding-top: 0px;padding-bottom: 0px; padding-left: 0px; padding-right: 0px; height: 80px; border-left: 0px;" value="${entry.event}" id="myText" class="textareaClass description"/>
+			</td>
+			<td class="pt-0 pb-0 pl-0">
+				<button data-ref="txt-${entry.id}" class="saveBtn btn btn-lg bg-info text-white btn-outline-info waves-effect" style= "height: 80px; width: 100px;"><i class="fas fa-thumbtack" aria-hidden="true"></i></button>
+			</td>
 		</tr>`);
 	});
 
-	/**
-	 * Develop the functionality to handle the color in each timeblock
-	 * to indicate whether it is in the past, present, or future.
-	 */
-
 	//TODO: the block timeblock when is past save button event
-	$(".saveBtn").on("click", function () {
-		if (
-			$(this).attr("data-textareaId") === $(".textareaClass").attr("dataID")
-		) {
-			var textareaInput = $(".textareaClass").val();
-			if (textareaInput !== "") {
-				$(".textareaClass").attr("disabled", "disabled");
-			} else {
-				textareaInput = $(".textareaClass").val();
-			}
-		}
-	});
 });
